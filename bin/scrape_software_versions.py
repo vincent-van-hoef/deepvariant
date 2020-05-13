d3fd8a28bee14fc96a4c5fb483bbe5af0b985e6f
Vincent van Hoef <vinva957@bmc-02c806clvdl.lan>
2020-05-13 15:06:32 +0200

    add utf-8; error in python3.5

--- bin/scrape_software_versions.py
+++ bin/scrape_software_versions.py
@@ -28,7 +28,7 @@ results['Bzip2'] = '<span style="color:#999999;\">N/A</span>'
 # Search each file using its regex
 for k, v in regexes.items():
     with open(v[0]) as x:
-        versions = x.read()
+        versions = x.read().decode('utf-8')
         match = re.search(v[1], versions)
         if match:
             results[k] = "v{}".format(match.group(1))
